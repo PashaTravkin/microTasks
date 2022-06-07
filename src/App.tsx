de5edc3d129f793ccf8261787878af5104df7d7a
PashaTravkin <refueller2022@bk.ru>
2022-06-07 16:38:32 +0300

    Create Input.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {FullInput} from "./components/FullInput";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [message, setMessage] = useState([
+        {message: "message1"},
+        {message: "message2"},
+        {message: "message3"},
+    ])
+
+    const addMessage =(title:string)=>{
+        setMessage([{message:title},...message])
+    }
+
+
+    return (
+        <div className={'App'}>
+            <FullInput addMessage = {addMessage}/>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message} </div>
+                )
+            })}
+        </div>
+    )
+
 }
 
 export default App;
